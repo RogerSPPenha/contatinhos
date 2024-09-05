5896ccbec59bd055d958dd649178fdbc12bde6ab
RogerSPPenha <rogerpenha28@gmail.com>
2024-09-05 13:57:35 -0300

    :lipstick: feat: estilizar input e font

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,6 +1,15 @@
 export const colors = {
-    white: "FFFFFF",
-    black: "000000",
+    white: "#FFFFFF",
+    black: "#000000",
 
-    blue: "4442f0",
+    blue: "#4442f0",
+
+    /*escala cinza*/
+    gray_100: "#EBEBEB",
+    gray_200: "#C4C4C4",
+    gray_300: "#8D8D99",
+    gray_400: "#7C7C8A",
+    gray_800: "#161616",
+
+
 }
