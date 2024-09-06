613bbcf7a390e4736c9a83aeb4ead590d00092c1
RogerSPPenha <rogerpenha28@gmail.com>
2024-09-06 14:11:55 -0300

    :sparkles: feat: adicionar usuário

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -17,7 +17,10 @@ export function Home(){
                     <Feather name="x" size={19} color={theme.colors.gray_300} onPress={() => setName("")}></Feather>
                 </Input>
             </View>
-            <Contact/>
+            <Contact contact={{
+                name: "Pedroso",
+                image: require("@/assets/avatar.jpeg")
+            }}/>
         </View>
     )
 }
